a24a805aa97a59ef5bdccd34fd7dcca496f54840
Andrew Yeung <71157672+Andrew-Yeung@users.noreply.github.com>
2022-03-05 12:09:22 -0500

    Create COCI '10 Contest 1 #5 Tabovi.java

---
+++ COCI '10 Contest 1 #5 Tabovi.java
@@ -0,0 +1,129 @@
+import java.util.*;
+import java.io.*;
+
+public class Tabovi {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = readInt();
+        int[] p = new int[n+1];
+        int[] d = new int[n+2];
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            p[i]=readInt();
+        }
+        for(int i=1;i<=n;i++){
+            d[i]=readInt()-p[i];
+        }
+        for(int i=1;i<=n+1;i++){
+            int diff=d[i]-d[i-1];
+            if(diff>0)ans+=diff;
+        }
+        System.out.println(ans);
+    }
+
+    static class pair {
+        int r, c;
+
+        pair(int r0, int c0) {
+            r = r0;
+            c = c0;
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+
+    static int quickpow(int x, int exp, int mod) {
+        //function gets exponents really fast
+        if (exp == 0) {
+            return 1;
+        }
+        int t = quickpow(x, exp / 2, mod);
+        t = t * t % mod;
+        if (exp % 2 == 0) return t;
+        return t * x % mod;
+    }
+
+    static int gcd(int a, int b) {
+        if (a == 0) return b;
+        return gcd(b % a, a);
+    }
+
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    static boolean contains(String[] arr, String target) {
+        boolean flag = false;
+        if (arr.length > 0) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i].equals(target)) {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+
+    static boolean prime(int n) {
+        if (n <= 1) return false;
+        else if (n == 2) return true;
+        else if (n % 2 == 0) return false;
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    //dsu
+    static int find_set(int p[], int d) { //parent array and the target parent you're looking for
+        if (d != p[d]) p[d] = find_set(p, p[d]);
+        return p[d];
+    }
+
+    static void union(int p[], int x, int y) {
+        int a = find_set(p, x), b = find_set(p, y);
+        p[a] = b;
+    }
+
+    public static class edge implements Comparable<edge> {
+        int u, v, w;
+
+        edge(int u0, int v0, int w0) {
+            u = u0;
+            v = v0;
+            w = w0;
+        }
+
+        public int compareTo(edge x) {
+            return w - x.w;
+        }
+    }
+}
